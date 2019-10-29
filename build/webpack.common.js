47e44f1ff0713c2afd1f397efbe0e5eecf732b9a
starkfire <kingbeyondthenorth@gmail.com>
2019-10-29 23:47:25 +0800

    add build scripts

---
+++ build/webpack.common.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
+
+module.exports = {
+	entry: './src/main.js',
+	output: {
+		path: path.resolve(__dirname, '../dist'),
+		publicPath: '/dist/',
+		filename: 'build.js'
+	},
+	module: {
+		rules: [
+			{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.vue$/, loader: 'vue-loader' },
+            { test: /\.css$/, use: ['vue-style-loader', 'css-loader'] },
+            { test: /\.scss$/, use: ['vue-style-loader', 'css-loader', 'sass-loader'] },
+            { test: /\.(jpg|png|svg|webp)$/, loader: 'file-loader'}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		}),
+		new VueLoaderPlugin(),
+		new webpack.HotModuleReplacementPlugin()
+	]
+}
