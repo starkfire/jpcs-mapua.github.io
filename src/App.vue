99d7c8c208f919594a207f7a7b77490873c69564
starkfire <kingbeyondthenorth@gmail.com>
2019-10-31 15:25:00 +0800

    add App.vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<Mixer></Mixer>
+</template>
+<script>
+import Vue from 'vue';
+import Vuikit from 'vuikit';
+import VuikitIcons from '@vuikit/icons';
+import '@vuikit/theme';
+import Mixer from './views/Mixer.vue';
+
+Vue.use(Vuikit);
+Vue.use(VuikitIcons);
+
+export default {
+	name: 'app',
+	components: {
+		'Mixer': Mixer
+	}
+};
+</script>
