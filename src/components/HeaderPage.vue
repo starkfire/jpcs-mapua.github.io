c2629f31a2e26488cf75a5b648439fa9d2ebd533
starkfire <kingbeyondthenorth@gmail.com>
2019-10-31 15:31:10 +0800

    tentative landing page

---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="landing-page">
+		<vk-navbar transparent>
+			<vk-navbar-nav slot="center">
+				<img class="navbar-jpcs-logo" src="../assets/images/jpcs.png">
+			</vk-navbar-nav>
+			<vk-navbar-nav slot="right">
+				<vk-navbar-nav-item title="Register"></vk-navbar-nav-item>
+			</vk-navbar-nav>
+		</vk-navbar>
+		<p class="landing-title">Startup Mixer 3.0</p>
+		<p class="landing-subtitle">Seminar Room, Mapua University, Muralla St, Intramuros, Manila</p>
+	</div>
+</template>
+<script>
+export default {
+	name: 'HeaderPage'
+};
+</script>
