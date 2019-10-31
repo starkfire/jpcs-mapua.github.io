dfbe0cf5bcc1d6f1f60d96a27a38431332a1f9ae
starkfire <kingbeyondthenorth@gmail.com>
2019-10-31 15:33:08 +0800

    add Intro components

---
+++ src/components/Intro.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<vk-grid class="uk-child-width-expand@s uk-text-center">
+			<div class="intro-page-white">
+				<h1 class="intro-page-header">Lorem Ipsum</h1>
+				<h2 class="intro-page-description">Lorem Ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt</h2>
+			</div>
+			<div class="intro-page">
+
+			</div>
+		</vk-grid>
+	</div>
+</template>
+<script>
+	
+export default{
+	name: 'Intro'
+};
+
+</script>
---
+++ src/components/IntroTwo.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<vk-grid class="uk-child-width-expand@s uk-text-center">
+		<div class="intro-two-page">
+
+		</div>
+		<div class="intro-two-white">
+			<h1 class="intro-two-page-header">Lorem Ipsum</h1>
+			<h2 class="intro-two-page-description">Lorem Ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt</h2>
+		</div>
+		</vk-grid>
+	</div>
+</template>
+<script>
+	
+export default{
+	name: 'IntroTwo'
+};
+
+</script>
