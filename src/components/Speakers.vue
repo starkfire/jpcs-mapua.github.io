086f1227be3de82fc757f5319d13f6ba4d8716d4
starkfire <kingbeyondthenorth@gmail.com>
2019-11-02 04:28:29 +0800

    tentative - speakers

---
+++ src/components/Speakers.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="speakers-page">
+		<h1 class="speakers-intro">Speakers</h1>
+		<vk-grid class="uk-child-width-expand@s uk-text-center" id="speakers-row">
+			<div class="speaker">
+				<img class="speaker-avatar" src="../assets/images/SPK_5.jpg">
+				<p class="speaker-name">Cristopher David</p>
+				<p class="speaker-details">Co-Founder & Chief Technology Officer<br>Veer Immersive Technologies</p>
+			</div>
+			<div class="speaker">
+				<img class="speaker-avatar" src="../assets/images/SPK_6.jpg">
+				<p class="speaker-name">Chiara Amisola</p>
+				<p class="speaker-details">Founder<br>Developh</p>
+			</div>
+			<div class="speaker">
+				<img class="speaker-avatar" src="../assets/images/SPK_3.jpg">
+				<p class="speaker-name">Paolo Espiritu</p>
+				<p class="speaker-details">Founder & Chief Hacker<br>Haraya Learning Innovations</p>
+			</div>
+		</vk-grid>
+		<vk-grid class="uk-child-width-expand@s uk-text-center" id="speakers-row">
+			<div>
+				<img class="speaker-avatar" src="../assets/images/SPK_7.jpg">
+				<p class="speaker-name">Ryan Joeyson Uy</p>
+				<p class="speaker-details">Product and Marketing Head<br>Starpay</p>
+			</div>
+			<div>
+				<img class="speaker-avatar" src="../assets/images/SPK_2.jpg">
+				<p class="speaker-name">Dexter Bano</p>
+				<p class="speaker-details">Founder & Chief Executive Officer<br>Orbital Exploration</p>
+			</div>
+			<div>
+				<img class="speaker-avatar" src="../assets/images/SPK_8.jpg">
+				<p class="speaker-name">Ramon Lapitan</p>
+				<p class="speaker-details">Customer Experience Manager<br>Kalibrr</p>
+			</div>
+		</vk-grid>
+	</div>
+</template>
+<script>
+export default {
+	name: 'Speakers'
+};
+</script>
