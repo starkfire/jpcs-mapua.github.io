678a8120d6bb7f29b441ef62b0d36be5d97515cb
starkfire <kingbeyondthenorth@gmail.com>
2019-11-07 13:49:15 +0800

    HeaderPage - tentative

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -5,11 +5,18 @@
 				<img class="navbar-jpcs-logo" src="../assets/images/jpcs.png">
 			</vk-navbar-nav>
 			<vk-navbar-nav slot="right">
-				<vk-navbar-nav-item title="Register"></vk-navbar-nav-item>
+				<!--<vk-navbar-nav-item title="Register"></vk-navbar-nav-item>-->
+				<vk-button class="register-nav">Register</vk-button>
 			</vk-navbar-nav>
 		</vk-navbar>
-		<p class="landing-title">Startup Mixer 3.0</p>
-		<p class="landing-subtitle">Seminar Room, Mapua University, Muralla St, Intramuros, Manila</p>
+		<vk-grid class="uk-child-width-expand@s uk-text-center">
+			<div>
+				<p class="landing-title">STARTCON 2019</p>
+				<p class="landing-subtitle">Mapua University, Muralla St, Intramuros, Manila</p>
+			</div>
+			<div>
+			</div>
+		</vk-grid>
 	</div>
 </template>
 <script>
