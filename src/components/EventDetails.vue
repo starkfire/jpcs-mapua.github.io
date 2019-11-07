f842f25cdb10d316e043556899918832374d9f9b
starkfire <kingbeyondthenorth@gmail.com>
2019-11-07 13:49:00 +0800

    EventDetails - tentative

--- src/components/EventDetails.vue
+++ src/components/EventDetails.vue
@@ -1,10 +1,15 @@
 <template>
-	
+	<div class="eventdetails-page">
+		<vk-grid class="uk-child-width-expand@s uk-text-center">
+			<div class="eventdetails-content">
+				<p class="eventdetails-venue">Mapúa University, Manila</p>
+				<p class="eventdetails-time">Nov. 22, 2019 | 10:00 PHT </p>
+			</div>
+		</vk-grid>
+	</div>
 </template>
 <script>
-
 export default {
 	name: 'EventDetails'
 };
-
 </script>
