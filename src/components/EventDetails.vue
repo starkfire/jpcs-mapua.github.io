5496b9ecae51827c92be51c9a9eef7161fece134
starkfire <kingbeyondthenorth@gmail.com>
2019-11-08 17:18:06 +0800

    tweak EventDetails component

--- src/components/EventDetails.vue
+++ src/components/EventDetails.vue
@@ -4,12 +4,23 @@
 			<div class="eventdetails-content">
 				<p class="eventdetails-venue">Mapúa University, Manila</p>
 				<p class="eventdetails-time">Nov. 22, 2019 | 10:00 PHT </p>
+				<vk-grid class="uk-child-width-expand@s uk-text-center">
+					<div></div>
+					<div><Countdown starttime="Nov 8, 2019 16:40:00" endtime="Nov 22, 2019 12:00:00"></Countdown></div>
+					<div></div>
+				</vk-grid>
 			</div>
 		</vk-grid>
 	</div>
 </template>
 <script>
+import Countdown from './Countdown.vue';
+
 export default {
-	name: 'EventDetails'
+	name: 'EventDetails',
+	components: {
+		Countdown
+	}
 };
+
 </script>
