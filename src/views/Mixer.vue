eac76c30a4dabe968ca43f2b11cffb2277dcb0f7
starkfire <kingbeyondthenorth@gmail.com>
2019-11-08 21:20:30 +0800

    rearrange components

--- src/views/Mixer.vue
+++ src/views/Mixer.vue
@@ -1,35 +1,23 @@
 <template>
 	<div>
 		<HeaderPage></HeaderPage>
-		<Partners></Partners>
 		<EventDetails></EventDetails>
-		<Intro></Intro>
-		<IntroTwo></IntroTwo>
-		<Speakers></Speakers>
-		<Team></Team>
+		<Schedule></Schedule>
 		<Footer></Footer>
 	</div>
 </template>
 <script>
 import HeaderPage from '../components/HeaderPage.vue';
-import Partners from '../components/Partners.vue';
 import EventDetails from '../components/EventDetails.vue';
-import Intro from '../components/Intro.vue';
-import IntroTwo from '../components/IntroTwo.vue';
-import Speakers from '../components/Speakers.vue';
-import Team from '../components/Team.vue';
+import Schedule from '../components/Schedule.vue';
 import Footer from '../components/Footer.vue';
 
 export default {
 	name: 'Mixer',
 	components: {
 		'HeaderPage': HeaderPage,
-		'Partners': Partners,
 		'EventDetails': EventDetails,
-		'Intro': Intro,
-		'IntroTwo': IntroTwo,
-		'Speakers': Speakers,
-		'Team': Team,
+		'Schedule': Schedule,
 		'Footer': Footer
 	}
 };
